1dd52c614ae302c19d0ee375ce88c5e9f7a7feed
ahmadzouikli123 <zouikliahmad68@gmail.com>
2026-03-29 14:56:47 -0600

    fix use client directive order

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic'
 "use client"
+export const dynamic = 'force-dynamic'
 import dynamic from "next/dynamic"
 const SchoolApp = dynamic(() => import("@/components/SchoolApp"), { ssr: false })
 export default function Page() { return <SchoolApp /> }
