4b078d39a7f1a8ce2bcb699ac84bee43bc206c74
ahmadzouikli123 <zouikliahmad68@gmail.com>
2026-04-05 18:08:09 -0600

    chore: ignore script files and txt files

--- fix_react.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/components/SchoolApp.jsx', 'utf8');
-
-content = content.replace(/React\.useState/g, 'useState');
-
-fs.writeFileSync('src/components/SchoolApp.jsx', content, 'utf8');
-console.log('Fixed!');
